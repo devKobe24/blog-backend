cd18166a75c8f3afdc5b912fd767769fc348c79d
Kobe <dev.skyachieve91@gmail.com>
2025-07-24 07:25:36 +0900

    feat: findPopularTags(Pageable pageable), searchTagsByName(@Param("keyword") String keyword, Pageable pageable) 추가
    - findPopularTags(Pageable pageable): 게시글(Post)에 많이 연결된 태그를 기준으로 인기 순으로 정렬된 태그 목록을 조회함.
    - searchTagsByName(@Param("keyword") String keyword, Pageable pageable): 태그 이름에 특정 키워드가 포함된 태그를 오름차순으로 정렬하여 검색함.

--- src/main/java/com/blog/repository/TagRepository.java
+++ src/main/java/com/blog/repository/TagRepository.java
@@ -1,6 +1,8 @@
 package com.blog.repository;
 
 import com.blog.entity.Tag;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -20,6 +22,12 @@ public interface TagRepository extends JpaRepository<Tag, Long> {
 
 	@Query("SELECT t FROM Tag t ORDER BY t.name ASC")
 	List<Tag> findAllOrderByName();
+
+	@Query("SELECT t FROM Tag t LEFT JOIN t.posts p GROUP BY t ORDER BY COUNT(p) DESC")
+	List<Tag> findPopularTags(Pageable pageable);
+
+	@Query("SELECT t FROM Tag t WHERE LOWER(t.name) LIKE LOWER(CONCAT('%', :keyword, '%')) ORDER BY t.name ASC")
+	Page<Tag> searchTagsByName(@Param("keyword") String keyword, Pageable pageable);
 
 	@Query("SELECT t FROM Tag t WHERE t.name LIKE %:keyword%")
 	List<Tag> searchByName(@Param("keyword") String keyword);
