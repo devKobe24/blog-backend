bc35c297c71c09e456112e14312ec70bee52c277
Kobe <dev.skyachieve91@gmail.com>
2025-07-27 07:37:23 +0900

    feat(comment-request): 댓글 수정 요청 필드에 유효성 검증 추가.
    - content 필드에 @NotBlank 및 @Size(min=1, max=1000) 애너테이션을 추가하여 빈 문자열이나 과돟한 입력을 방지합니다.
    - 사용자 입력값에 대한 서버 검증 강화를 통해 안정적인 API 사용을 유도합니다.

--- src/main/java/com/blog/dto/request/CommentUpdateRequest.java
+++ src/main/java/com/blog/dto/request/CommentUpdateRequest.java
@@ -1,5 +1,7 @@
 package com.blog.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -8,5 +10,8 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class CommentUpdateRequest {
+
+	@NotBlank(message = "댓글 내용은 필수입니다.")
+	@Size(min = 1, max = 1000, message = "댓글 내용은 1자 이상 1000자 이하여야 합니다.")
 	private String content;
 }
