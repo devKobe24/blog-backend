a411849a86f38caf40e4ead03168376d20858980
Kobe <dev.skyachieve91@gmail.com>
2025-07-27 07:19:37 +0900

    feat(category): 카테고리 생성 요청 DTO에 유효성 검증 및 메시지 한글화.
    - @NotBlank, @Size를 적용하여 name과 description 필드의 입력값 검증을 추가하였고, 오류 메시지를 한글로 작성하였습니다.

--- src/main/java/com/blog/dto/request/CategoryCreateRequest.java
+++ src/main/java/com/blog/dto/request/CategoryCreateRequest.java
@@ -1,5 +1,7 @@
 package com.blog.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -8,6 +10,11 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class CategoryCreateRequest {
+
+	@NotBlank(message = "카테고리명은 필수입니다.")
+	@Size(min = 1, max = 50, message = "카테고리명은 1자 이상 50자 이하여야 합니다.")
 	private String name;
+
+	@Size(max = 200, message = "카테고리 설명은 200자 이하여야 합니다.")
 	private String description;
 }
