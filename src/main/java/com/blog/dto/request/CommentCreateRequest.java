db86a0358486cb171f3ce2c6e82f816dab1255be
Kobe <dev.skyachieve91@gmail.com>
2025-07-27 07:33:29 +0900

    feat(comment-request): 댓글 생성 요청 필드에 유효성 검증 추가
    - content 필등에 @NotBlank 및 @Size(min=1, max=1000) 애너테이션을 추가하여 공백 또는 과도한 길이의 입력을 제한하도록 설정하였습니다.
    - 입력 유효성 강화를 통해 서버 측  데이터 안정성과 사용자 피드백 경험을 향상시켰습니다.

--- src/main/java/com/blog/dto/request/CommentCreateRequest.java
+++ src/main/java/com/blog/dto/request/CommentCreateRequest.java
@@ -1,5 +1,7 @@
 package com.blog.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -8,6 +10,10 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class CommentCreateRequest {
+
+	@NotBlank(message = "댓글 내용은 필수입니다.")
+	@Size(min = 1, max = 1000, message = "댓글 내용은 1자 이상 1000자 이하여야 합니다.")
 	private String content;
+
 	private Long parentId;
 }
