8f3eb6510b52472b29000353fc59f8060e226c40
Kobe <dev.skyachieve91@gmail.com>
2025-07-27 09:35:48 +0900

    feat(user-dto): 로그인 요청 DTO에 유효성 검증 추가.
    - username, password 필드에 @NotBlank 애노테이션 적용하여 입력값 누락을 방지합니다.
    - 로그인 시 필수 입력값을 명확히 하여 API의 안정성과 신뢰성을 향상시켰습니다.

--- src/main/java/com/blog/dto/request/UserLoginRequest.java
+++ src/main/java/com/blog/dto/request/UserLoginRequest.java
@@ -1,5 +1,6 @@
 package com.blog.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Getter
@@ -8,6 +9,10 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class UserLoginRequest {
+
+	@NotBlank(message = "사용자명은 필수입니다.")
 	private String username;
+
+	@NotBlank(message = "비밀번호는 필수입니다.")
 	private String password;
 }
