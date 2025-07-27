b86d005db8af72d5b34071db838af003d9a1d990
Kobe <dev.skyachieve91@gmail.com>
2025-07-27 09:00:39 +0900

    feat(post): 게시물 수정 요청에 유효성 검증 추가
    - 제목, 내용, 태그 이름 등에 대해 @Size 기반의 유효성 검증을 추가하였습니다.
    - 클라이언트에서 잘못된 입력값을 방지하고 서버 측 유효성 검사를 강화하였습니다.

--- src/main/java/com/blog/dto/request/PostUpdateRequest.java
+++ src/main/java/com/blog/dto/request/PostUpdateRequest.java
@@ -1,5 +1,6 @@
 package com.blog.dto.request;
 
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.util.List;
@@ -10,9 +11,17 @@ import java.util.List;
 @AllArgsConstructor
 @Builder
 public class PostUpdateRequest {
+
+	@Size(min = 1, max = 200, message = "제목은 1자 이상 200자 이하여야 합니다.")
 	private String title;
+
+	@Size(min = 10, max = 10000, message = "내용은 10자 이상 10000자 이하여야 합니다.")
 	private String content;
+
 	private Long categoryId;
-	private List<String> tagNames;
+
+	@Size(max = 10, message = "태그는 최대 10개까지 추가할 수 있습니다.")
+	private List<@Size(max = 20, message = "태그명은 20자 이하여야 합니다.") String> tagNames;
+
 	private boolean isPublished;
 }
