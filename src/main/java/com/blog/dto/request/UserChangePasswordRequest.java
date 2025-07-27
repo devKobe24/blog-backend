aa667481f6fb9565b4881f7c5f4d23420d69ccd8
Kobe <dev.skyachieve91@gmail.com>
2025-07-27 09:24:10 +0900

    feat(user-dto): UserChangePasswordRequest에 비밀번호 검증 로직 추가.
    - currentPassword, newPassword 필드에 대해 @NotBlank 등 기본 검증 어노테이션을 추가하였습니다.
    - newPassword에는 길이 제한과 함께 영문, 숫자, 특수문자를 포함해야 한다는 정규표현식 기반의 @Pattern 제약을 적용하여 보안성을 강홯하였습니다.

--- src/main/java/com/blog/dto/request/UserChangePasswordRequest.java
+++ src/main/java/com/blog/dto/request/UserChangePasswordRequest.java
@@ -1,5 +1,8 @@
 package com.blog.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -8,6 +11,13 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class UserChangePasswordRequest {
+
+	@NotBlank(message = "현재 비밀번호는 필수입니다.")
 	private String currentPassword;
+
+	@NotBlank(message = "새 비밀 번호는 필수입니다.")
+	@Size(min = 8, max = 100, message = "새 비밀번호는 8자 이상 100자 이하여야 합니다.")
+	@Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\\\\-=\\\\[\\\\]{};':\\\"\\\\\\\\|,.<>\\\\/?]).*$",
+	message = "새 비밀번호는 영문, 숫자, 특수문자를 포함해야 합니다.")
 	private String newPassword;
 }
